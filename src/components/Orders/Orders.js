df0d38816e28230ea2a74b9cf76cb53deb11692b
Jeff Cook <cookturingedu@gmail.com>
2023-01-17 12:04:38 -0500

    Add orders to Apps state and add key to map over orders component

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -4,13 +4,14 @@ import './Orders.css';
 const Orders = props => {
   const orderEls = props.orders.map(order => {
     return (
-      <div className="order">
+      <div className="order" key={order.id}>
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
+            return <li key={Math.random()}>{ingredient}</li>
           })}
         </ul>
+        <button className='delete-button' onClick={() => props.trashOrder(order.id)}>🗑</button>
       </div>
     )
   });
